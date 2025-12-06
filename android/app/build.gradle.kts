867e123040f7f4787ccb0d3a53076fcc7bc444ca
swangai7178 <swangai7178@gmail.com>
2025-12-06 09:27:35 +0300

    update icon

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.ffm_wrapper"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    //ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
